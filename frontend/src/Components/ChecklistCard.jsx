a53b4321138d5d3c9aa834a664395c2686014813
emmanyouwell <emingala02@gmail.com>
2024-02-21 00:49:56 +0800

    Responsive ChecklistCard

--- frontend/src/Components/ChecklistCard.jsx
+++ frontend/src/Components/ChecklistCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ChecklistCard = ({ img, title, description, percent, color}) => {
     return (
-        <div className="card min-w-24 max-w-96 bg-base-100 drop-shadow-xl">
-            <div className="relative">
+        <div className="card min-w-24 max-w-96 bg-base-100 drop-shadow-xl h-full">
+            <div className="relative max-h-64">
 
                 <figure className="bg-base-200">
-                    <img src={img} alt="Shoes" className="max-w-96 h-64 object-contain" />
+                    <img src={img} alt="Shoes" className="max-w-64 max-h-64 object-center" />
                     {/* <div className="min-h-full bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${img})`}}></div> */}
                 </figure>
 
@@ -17,7 +17,6 @@ const ChecklistCard = ({ img, title, description, percent, color}) => {
             <div className="card-body">
                 <h2 className="card-title">
                     {title}
-                    
                 </h2>
                 <div className={`badge badge-${color} text-white`}>{percent}% recommended</div>
                 <p>{description}</p>
