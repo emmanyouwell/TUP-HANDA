23bf15b6d97ee75066c3282cadbdcf77a5c252b0
emmanyouwell <emingala02@gmail.com>
2024-02-23 01:14:42 +0800

    Start ng forgot password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,8 +29,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        <ToastContainer position="bottom-right"/>
       </ScrollToTop>
-      <ToastContainer />
+      
     </BrowserRouter>
   )
 }
---
+++ frontend/src/Views/Users/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+const ForgotPassword = () => {
+  return (
+    <div>ForgotPassword</div>
+  )
+}
+
+export default ForgotPassword
