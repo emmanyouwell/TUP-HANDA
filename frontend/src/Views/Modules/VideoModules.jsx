e8bcc56a8826f7f5d3643d9cb29e426be1d65f25
emmanyouwell <emingala02@gmail.com>
2024-03-01 13:36:25 +0800

    Adjusted cols in video modules

--- frontend/src/Views/Modules/VideoModules.jsx
+++ frontend/src/Views/Modules/VideoModules.jsx
@@ -16,7 +16,7 @@ const VideoModules = () => {
             {/* <Navbar /> */}
             <div className="container mx-auto p-10 mt-10">
                 <h1 className="mb-20 font-bold text-3xl md:text-4xl lg:text-5xl font-[Poppins]">Watch our tutorials!</h1>
-                <div className="grid justify-items-center items-center justify-center gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                <div className="grid justify-items-center items-center justify-center gap-10 md:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
 
                     {videos.map(video => (
                         <VideoCard key={video._id} link={video.videoLink} title={video.title} shortDesc={video.shortDesc} />
