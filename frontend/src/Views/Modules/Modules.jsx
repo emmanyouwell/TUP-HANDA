3506ffa39f6f3d6205858ddb86cf1195807971a8
emmanyouwell <emingala02@gmail.com>
2024-03-04 09:19:10 +0800

    added loader

--- frontend/src/Views/Modules/Modules.jsx
+++ frontend/src/Views/Modules/Modules.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../Components/Navbar'
 import ModuleCard from '../../Components/ModuleCard'
 import { useDispatch, useSelector } from 'react-redux'
 import { getModules, clearErrors } from '../../Actions/modulesActions'
+import Loader from '../../Components/Loader'
 const Modules = () => {
   const dispatch = useDispatch()
   const { modules, loading, error } = useSelector(state => state.modules)
@@ -15,18 +16,10 @@ const Modules = () => {
   },[dispatch, error])
   return (
     <>
-      {/* <Navbar /> */}
       <div className="container mx-auto  p-10 mt-10">
-        <h1 className="mb-20 font-bold text-3xl md:text-4xl lg:text-5xl font-[Poppins]">Read our downloadable modules!</h1>
-        
-          {modules && modules.length > 0 ? <div className="grid justify-items-center items-center justify-center gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">{modules.map(m=> <ModuleCard key={m._id} img={m.img.url} link={m.file.url} title={m.title} description={m.description} shortDesc={m.shortDesc} /> )}</div>: <p>No modules yet</p>}
-         
-          
-
+        {loading ? <div className="min-h-screen"><Loader/></div> : <> <h1 className="mb-20 font-bold text-3xl md:text-4xl lg:text-5xl font-[Poppins]">Read our downloadable modules!</h1>
+          {modules && modules.length > 0 ? <div className="grid justify-items-center items-center justify-center gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">{modules.map(m=> <ModuleCard key={m._id} img={m.img.url} link={m.file.url} title={m.title} description={m.description} shortDesc={m.shortDesc} /> )}</div>: <p>No modules yet</p>}</>}
        
-
-
-
       </div>
 
     </>
--- frontend/src/Views/Modules/VideoModules.jsx
+++ frontend/src/Views/Modules/VideoModules.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import VideoCard from '../../Components/VideoCard'
 import {useDispatch, useSelector} from 'react-redux'
 import { getVideos, clearErrors } from '../../Actions/videoActions'
+import Loader from '../../Components/Loader'
 const VideoModules = () => {
     const dispatch = useDispatch()
     const {videos, loading, error} = useSelector(state => state.videos)
@@ -15,17 +16,16 @@ const VideoModules = () => {
         <>
             {/* <Navbar /> */}
             <div className="container mx-auto p-10 mt-10">
+                {loading ? <div className="min-h-screen"><Loader /></div> : <>
                 <h1 className="mb-20 font-bold text-3xl md:text-4xl lg:text-5xl font-[Poppins]">Watch our tutorials!</h1>
                 <div className="grid justify-items-center items-center justify-center gap-10 md:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
-
                     {videos.map(video => (
                         <VideoCard key={video._id} link={video.videoLink} title={video.title} shortDesc={video.shortDesc} />
                     ))}
-                    
-                   
-                   
-
                 </div>
+                
+                </>}
+               
 
             </div>
 
