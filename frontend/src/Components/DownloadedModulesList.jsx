f47475e215ed086ea885aefc7e068e4f828bd603
emmanyouwell <emingala02@gmail.com>
2024-03-05 16:35:09 +0800

    implementation sa frontend

---
+++ frontend/src/Components/DownloadedModulesList.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import {
+    Timeline,
+    TimelineItem,
+    TimelineConnector,
+    TimelineHeader,
+    TimelineIcon,
+    TimelineBody,
+    Typography,
+    Avatar,
+} from "@material-tailwind/react";
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserCourse } from '../Actions/userActions';
+import { Link } from 'react-router-dom';
+export function DownloadedModulesList({ list }) {
+    const { modules } = useSelector(state => state.myModules)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getUserCourse())
+    }, [])
+
+    return (
+        <div className="w-[32rem]">
+            <Timeline>
+                {modules && modules.downloadedModules ? modules.downloadedModules.map((item, index) => <TimelineItem>
+                    {index + 1 == modules.downloadedModules.length ? '' : <TimelineConnector />}
+                    <TimelineHeader>
+                        <TimelineIcon className="p-0">
+                            <Avatar size="sm" src={item.img.url} alt="user 1" withBorder />
+                        </TimelineIcon>
+                        <Typography variant="h5" color="blue-gray">
+                        
+                            {item.title}
+                        </Typography>
+                    </TimelineHeader>
+                    <TimelineBody className="pb-8">
+                        <Typography color="gary" className="font-normal text-gray-600">
+                            {item.shortDesc}
+                        </Typography>
+                    </TimelineBody>
+                </TimelineItem>) : <p>No downloaded modules yet</p>}
+
+
+            </Timeline>
+        </div>
+    );
+}
--- frontend/src/Views/Users/Profile.jsx
+++ frontend/src/Views/Users/Profile.jsx
@@ -1,131 +1,152 @@
-import { Avatar, Typography, Button } from "@material-tailwind/react";
+import {
+  Card,
+  CardBody, Avatar, Typography, Button
+} from "@material-tailwind/react";
 import {
   MapPinIcon,
   BriefcaseIcon,
   BuildingLibraryIcon,
 } from "@heroicons/react/24/solid";
-import React, {useEffect} from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getProfile, clearErrors } from "../../Actions/userActions";
-import Navbar from "../../Components/Navbar";
-import {Link} from 'react-router-dom'
-import { getUser } from "../../utils/helper";
+
+
+
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom'
 import Loader from "../../Components/Loader";
+import { DownloadedModulesList } from "../../Components/DownloadedModulesList";
 export function Profile() {
-    
-    const { user, error, loading } = useSelector(state => state.auth)
+
+  const { user, error, loading } = useSelector(state => state.auth)
 
   return (
     <>
+
       {loading ? <div className="min-h-screen flex justify-center items-center container mx-auto">
-      <Loader/>
-      </div>  : <> 
-      <section className="relative block h-[50vh] overflow-hidden">
-      <div style={{ backgroundImage: `url(${user && user.coverAvatar && user.coverAvatar[0] && user.coverAvatar[0].url ? user.coverAvatar[0].url : 'https://res.cloudinary.com/dtrr0ihcb/image/upload/v1708771983/TUPHANDA_COVER_PHOTO/xbgzqfwjtkrjacbpcely.png'})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="bg-profile-background absolute top-0 h-full w-full scale-105" />
-        <div className="absolute top-0 h-full w-full bg-black/25 bg-cover bg-center" />
-      </section>
-      <section className="relative bg-white py-16">
-        <div className="relative mb-6 -mt-40 flex w-full px-4 min-w-0 flex-col break-words bg-white">
-          <div className="container mx-auto">
-            <div className="flex flex-col lg:flex-row justify-between">
-              <div className="relative flex gap-6 items-start">
-                <div className="-mt-20 w-40">
-                  <Avatar
-                    src={user && user.avatar[0].url}
-                    alt="Profile picture"
-                    variant="circular"
-                    className="w-36 h-36 rounded-full object-cover"
-                  />
+        <Loader />
+      </div> : <>
+        <section className="relative block h-[50vh] overflow-hidden">
+          <div style={{ backgroundImage: `url(${user && user.coverAvatar && user.coverAvatar[0] && user.coverAvatar[0].url ? user.coverAvatar[0].url : 'https://res.cloudinary.com/dtrr0ihcb/image/upload/v1708771983/TUPHANDA_COVER_PHOTO/xbgzqfwjtkrjacbpcely.png'})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="bg-profile-background absolute top-0 h-full w-full scale-105" />
+          <div className="absolute top-0 h-full w-full bg-black/25 bg-cover bg-center" />
+        </section>
+        <section className="relative bg-white py-16">
+          <div className="relative mb-6 -mt-40 flex w-full px-4 min-w-0 flex-col break-words bg-white">
+            <div className="container mx-auto">
+              <div className="flex flex-col lg:flex-row justify-between">
+                <div className="relative flex gap-6 items-start">
+                  <div className="-mt-20 w-40">
+                    <Avatar
+                      src={user && user.avatar[0].url}
+                      alt="Profile picture"
+                      variant="circular"
+                      className="w-36 h-36 rounded-full object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col mt-2">
+                    <Typography variant="h4" color="blue-gray">
+                      {user && `${user.firstName} ${user.lastName}`}
+                    </Typography>
+                    <Typography variant="paragraph" color="gray" className="!mt-0 font-normal">{user && user.email}</Typography>
+                  </div>
                 </div>
-                <div className="flex flex-col mt-2">
-                  <Typography variant="h4" color="blue-gray">
-                    {user && `${user.firstName} ${user.lastName}`}
+
+                <div className="mt-10 mb-10 flex lg:flex-col justify-between items-center lg:justify-end lg:mb-0 lg:px-4 flex-wrap lg:-mt-5">
+                  <Link to="/profile/update"><Button className="bg-gray-900 w-fit lg:ml-auto">Edit Profile</Button></Link>
+                  <div className="flex justify-start py-4 pt-8 lg:pt-4">
+                    <div className="mr-4 p-3 text-center">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        2,000,000,000
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Friends
+                      </Typography>
+                    </div>
+                    <div className="mr-4 p-3 text-center">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        10
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Photos
+                      </Typography>
+                    </div>
+                    <div className="p-3 text-center lg:mr-4">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        89
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Comments
+                      </Typography>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+              <div className="-mt-4 container space-y-2">
+                <div className="flex items-center gap-2">
+                  <MapPinIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
+                  <Typography className="font-medium text-blue-gray-500">
+                    {user ? `${user.address}, ${user.city}, ${user.country} ${user.postalCode}` : "City, Country"}
                   </Typography>
-                  <Typography variant="paragraph" color="gray" className="!mt-0 font-normal">{user && user.email}</Typography>
+                </div>
+                <div className="flex items-center gap-2">
+                  <BriefcaseIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
+                  <Typography className="font-medium text-blue-gray-500">
+                    {user ? user.course : "Course"}
+                  </Typography>
+                </div>
+                <div className="flex items-center gap-2">
+                  <BuildingLibraryIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
+                  <Typography className="font-medium text-blue-gray-500">
+                    {user ? `${user.department}` : "Department"}
+                  </Typography>
                 </div>
               </div>
 
-              <div className="mt-10 mb-10 flex lg:flex-col justify-between items-center lg:justify-end lg:mb-0 lg:px-4 flex-wrap lg:-mt-5">
-                <Link to="/profile/update"><Button className="bg-gray-900 w-fit lg:ml-auto">Edit Profile</Button></Link>
-                <div className="flex justify-start py-4 pt-8 lg:pt-4">
-                  <div className="mr-4 p-3 text-center">
-                    <Typography
-                      variant="lead"
-                      color="blue-gray"
-                      className="font-bold uppercase"
-                    >
-                      2,000,000,000
-                    </Typography>
-                    <Typography
-                      variant="small"
-                      className="font-normal text-blue-gray-500"
-                    >
-                      Friends
-                    </Typography>
-                  </div>
-                  <div className="mr-4 p-3 text-center">
-                    <Typography
-                      variant="lead"
-                      color="blue-gray"
-                      className="font-bold uppercase"
-                    >
-                      10
-                    </Typography>
-                    <Typography
-                      variant="small"
-                      className="font-normal text-blue-gray-500"
-                    >
-                      Photos
-                    </Typography>
-                  </div>
-                  <div className="p-3 text-center lg:mr-4">
-                    <Typography
-                      variant="lead"
-                      color="blue-gray"
-                      className="font-bold uppercase"
-                    >
-                      89
-                    </Typography>
-                    <Typography
-                      variant="small"
-                      className="font-normal text-blue-gray-500"
-                    >
-                      Comments
-                    </Typography>
-                  </div>
-                </div>
+            </div>
 
-              </div>
+
+          </div>
+        </section>
+        <section>
+          <div className="mb-6 flex w-full px-4 min-w-0 flex-col bg-white">
+            <div className="container mx-auto">
+              <h1 className="font-[Poppins] font-medium text-2xl">Modules downloaded</h1>
+              <Card className="mt-6 w-1/2">
+                <CardBody className="overflow-scroll h-[500px]">
+                  <DownloadedModulesList/>
+                </CardBody>
+
+              </Card>
+
             </div>
-            <div className="-mt-4 container space-y-2">
-              <div className="flex items-center gap-2">
-                <MapPinIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
-                <Typography className="font-medium text-blue-gray-500">
-                  {user ? `${user.address}, ${user.city}, ${user.country} ${user.postalCode}` : "City, Country"}
-                </Typography>
-              </div>
-              <div className="flex items-center gap-2">
-                <BriefcaseIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
-                <Typography className="font-medium text-blue-gray-500">
-                {user ? user.course : "Course"}
-                </Typography>
-              </div>
-              <div className="flex items-center gap-2">
-                <BuildingLibraryIcon className="-mt-px h-4 w-4 text-blue-gray-500" />
-                <Typography className="font-medium text-blue-gray-500">
-                {user ? `${user.department}` : "Department"}
-                </Typography>
-              </div>
-            </div>
-            
           </div>
 
 
-        </div>
-      </section>
-      </> }
-      
-      
+        </section>
+      </>}
+
+
 
     </>
   );
