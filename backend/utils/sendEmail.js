060514ef646e6fcc199a33b71606f303d4e7fd1d
emmanyouwell <emingala02@gmail.com>
2024-03-07 10:00:17 +0800

    Amazon SES configuration sa sendEmail at .env

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,10 +4,14 @@ const sendEmail = async options => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        // secure: true,
         auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD
-        }
+        },
+        // tls:{
+        //     rejectUnauthorized: false
+        // }
     });
 
     const message = {
