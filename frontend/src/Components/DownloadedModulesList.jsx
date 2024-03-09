5d7e6c848d9c4111361b6d8d6d189d5455712d1f
emmanyouwell <emingala02@gmail.com>
2024-03-09 14:35:46 +0800

    Click title to preview

--- frontend/src/Components/DownloadedModulesList.jsx
+++ frontend/src/Components/DownloadedModulesList.jsx
@@ -12,8 +12,9 @@ import {
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserCourse } from '../Actions/userActions';
 import { Link } from 'react-router-dom';
+import Loader from './Loader';
 export function DownloadedModulesList() {
-    const { modules } = useSelector(state => state.myModules)
+    const { modules, loading } = useSelector(state => state.myModules)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getUserCourse())
@@ -22,16 +23,20 @@ export function DownloadedModulesList() {
     return (
         <div className="lg:w-[32rem] w-full">
             <Timeline>
-                {modules && modules.downloadedModules && modules.downloadedModules.length > 0 ? modules.downloadedModules.map((item, index) => <TimelineItem key={item._id}>
+                {loading ? <div className="min-h-screen mt-20"><Loader /></div> :modules && modules.downloadedModules && modules.downloadedModules.length > 0 ? modules.downloadedModules.map((item, index) => <TimelineItem key={item._id}>
                     {index + 1 == modules.downloadedModules.length ? '' : <TimelineConnector />}
                     <TimelineHeader>
+                        
                         <TimelineIcon className="p-0">
+                        <Link to='/modules'>
                             <Avatar size="sm" src={item.img.url} alt="user 1" withBorder />
+                        </Link>
                         </TimelineIcon>
+                       
                         <Typography variant="h5" color="blue-gray">
-                            <Link to="/modules">
-                                {item.title}
-                            </Link>
+                            <a href={item.file.url}>
+                            {item.title}
+                            </a>
                         </Typography>
                     </TimelineHeader>
                     <TimelineBody className="pb-8">
