19679acb0a57781045c93e7f811674208b0bc66a
emmanyouwell <emingala02@gmail.com>
2024-03-11 15:24:26 +0800

    Label correction

--- frontend/src/Views/Modules/Admin/AddQuestions.jsx
+++ frontend/src/Views/Modules/Admin/AddQuestions.jsx
@@ -93,7 +93,7 @@ const AddQuestions = () => {
                                                                     )}
                                                                 </FieldArray>
                                                                 <label htmlFor="Correct answer" className="block text-sm font-medium leading-6 text-gray-900">
-                                                                    Correct Question
+                                                                    Correct Answer
                                                                 </label>
                                                                 <Field name={`questions.${index}.correctAnswer`} placeholder="Correct Answer" className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mb-4" />
 
